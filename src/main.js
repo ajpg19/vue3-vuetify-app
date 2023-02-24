10dd5b8ead8ad48151ea8985b6918bf5c08c9ef2
Albertj PG <ajpg19@gmail.com>
2023-02-24 10:27:47 +0100

    add mdi font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 
 // Vuetify
@@ -13,7 +14,10 @@ const vuetify = createVuetify({
     directives,
     theme: {
         defaultTheme: 'dark'
-    }
+    },
+    icons: {
+        defaultSet: 'mdi' // This is already the default value - only for display purposes
+    },
 })
 
 createApp(App)
